43a2514c361d19344aa33262a6536252562ef822
Ilnurios325 <ipilulka@gmail.com>
2023-02-19 14:43:53 +0300

    work

---
+++ homeWork/solution.cs
@@ -0,0 +1,27 @@
+﻿void ShowArray(string[] array){
+     for (int i = 0; i < array.Length; i++)
+      {
+        Console.WriteLine(array[i]+"");
+      }
+      Console.WriteLine();
+    }
+     string[] finalArray = {"hello", "2222", "world", "555"};
+   
+    string[] emptyArray = new string [4];
+    
+    for (int i=0; i<finalArray.Length; i++)
+    {
+      if(finalArray[i].Length <=3)
+      {
+        
+        Console.WriteLine(finalArray[i]+", ");
+      }
+      
+      if(finalArray[0].Length > 3 && finalArray[1].Length > 3 && finalArray[2].Length > 3 && finalArray[3].Length > 3)
+      
+        Console.Write($"/ no results /"); 
+      
+    }
+     
+    ShowArray(emptyArray);
+
